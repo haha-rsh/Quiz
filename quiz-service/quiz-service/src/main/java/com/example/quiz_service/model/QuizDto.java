4e02420babd1880fc0ac9a8211e27a8bf1ab770c
Harsh Kamal <harshonn@gmail.com>
2024-12-17 15:00:15 +0530

    Final

---
+++ quiz-service/quiz-service/src/main/java/com/example/quiz_service/model/QuizDto.java
@@ -0,0 +1,10 @@
+package com.example.quiz_service.model;
+
+import lombok.Data;
+
+@Data
+public class QuizDto {
+    private String categoryName;
+    private Integer numQuestions;
+    private String title;
+}
